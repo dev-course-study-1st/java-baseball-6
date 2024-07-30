a099bb3b1a91ca46f6ea888196c06bc21ed0a3dd
junho <>
2024-07-30 23:21:08 +0900

    refactor: 파일명 변경에 따른 타입 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
+import baseball.controller.BaseBallController;
+
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
         try {
-            GameManager gameManager = new GameManager();
+            BaseBallController gameManager = new BaseBallController();
             gameManager.start();
         } catch (IllegalArgumentException e) {
             throw e;
