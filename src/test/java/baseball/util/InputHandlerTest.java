371a11d5a88d2feba2cf5c3f505ef3ea572e4598
jmd5314 <jmd5314@gmail.com>
2024-08-01 22:19:37 +0900

    test: 사용자 입력 테스트 코드 작성

---
+++ src/test/java/baseball/util/InputHandlerTest.java
@@ -0,0 +1,36 @@
+package baseball.util;
+
+import baseball.model.Numbers;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputHandlerTest {
+    @Test
+    public void 사용자입력테스트 () throws Exception{
+        //given
+        InputHandler inputHandler = new InputHandler();
+        InputStream input = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("456".getBytes());
+        System.setIn(in);
+        //when
+        Numbers numbers = inputHandler.inputNumbers();
+        //then
+        List<Integer> numberList = numbers.getNumbers();
+        // 3자리수 검증
+        assertEquals(3, numberList.size());
+        // 1~9 사이 수 검증
+        for(int number:numberList){
+            assertTrue(number >= 1 && number <= 9);
+        }
+        // 서로 다른 숫자 검증
+        Set<Integer> numberSet = new HashSet<>(numberList);
+        assertEquals(3, numberSet.size());
+     }
+}
