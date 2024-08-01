e048feab7d322397ff322cff109c4e002b0343f2
jmd5314 <jmd5314@gmail.com>
2024-08-02 02:48:42 +0900

    feat: 재시작 커멘드 검증 메소드 구현

--- src/main/java/baseball/util/InputHandler.java
+++ src/main/java/baseball/util/InputHandler.java
@@ -22,7 +22,7 @@ public class InputHandler {
     public String inputRestart(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
-
+        inputValidator.validCommend(input);
         return input;
     }
 }
--- src/main/java/baseball/util/InputValidator.java
+++ src/main/java/baseball/util/InputValidator.java
@@ -31,5 +31,8 @@ public class InputValidator {
         return true;
     }
     public void validCommend(String input){
+        if(!(input.equals("1")||input.equals("2"))){
+            throw new IllegalArgumentException("입력은 1 또는 2여야 합니다.");
+        }
     }
 }
