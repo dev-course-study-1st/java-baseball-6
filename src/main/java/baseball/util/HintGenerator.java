151e393f5ce360526f18026c0b7cec044c467921
jmd5314 <jmd5314@gmail.com>
2024-08-01 19:13:45 +0900

    feat: 힌트 생성 기능 구현

---
+++ src/main/java/baseball/util/HintGenerator.java
@@ -0,0 +1,26 @@
+package baseball.util;
+import java.util.List;
+public class HintGenerator {
+    public String generateHint(List<Integer> userNumbers, List<Integer> computerNumbers) {
+        int balls = 0;
+        int strikes = 0;
+        for (int i = 0; i < userNumbers.size(); i++) {
+            if (userNumbers.get(i).equals(computerNumbers)) {
+                strikes++;
+            } else if (computerNumbers.contains(userNumbers.get(i))) {
+                balls++;
+            }
+        }
+        return generateHintMessage(balls, strikes);
+    }
+    private String generateHintMessage(int balls,int strikes){
+        StringBuilder hint = new StringBuilder();
+        if(balls>0)
+            hint.append(balls + "볼 ");
+        if(strikes>0)
+            hint.append(strikes + "스트라이크");
+        if(balls==0&&strikes==0)
+            hint.append("낫싱");
+        return hint.toString().trim();
+    }
+}
