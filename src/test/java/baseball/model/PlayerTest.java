5755e975b698466d061b8fb6d4efd75636342849
junho <>
2024-08-02 20:02:18 +0900

    refactor: 사용자 입력값 오류 시 에러 출력 후 종료 테스트 위치 변경

--- src/test/java/baseball/model/PlayerTest.java
+++ src/test/java/baseball/model/PlayerTest.java
@@ -1,6 +1,25 @@
 package baseball.model;
 
-public class PlayerTest {
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerTest extends NsTest {
 
 
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+    @Test
+    void 예외_테스트_숫자가아닌_문자가_들어갔을_때() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1-4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 }
